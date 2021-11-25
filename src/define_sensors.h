95223e1c98b44c438df716010cc4802a8150874d
Daniel Serleg <daniel.serleg@bitrise.io>
2021-11-25 19:49:58 +0100

    Handle buttons

---
+++ src/define_sensors.h
@@ -0,0 +1,18 @@
+// Temperature meter
+#define DHTPIN 7      // what pin we're connected to
+#define DHTTYPE DHT22 // DHT 22  (AM2302)
+
+// Screen
+#define OLED_RESET 4
+
+//Software serial
+#define SOFT_S_RX 2
+#define SOFT_S_TX 3
+
+// Buttons
+#define SCREEN_BUTTON_PIN 8
+#define RESET_BUTTON_PIN 9
+
+// Soil moisture
+#define AIR_VALUE 615   //you need to replace this value with Value_1
+#define WATER_VALUE 249 //you need to replace this value with Value_2
--- src/main.cpp
+++ src/main.cpp
@@ -14,16 +14,8 @@
 #include "Display.h"
 #include "Wifi.h"
 
-//Constants
-#define DEBUG true
-
-#define DHTPIN 7      // what pin we're connected to
-#define DHTTYPE DHT22 // DHT 22  (AM2302)
-
-#define OLED_RESET 4
-
-#define SOFT_S_RX 2
-#define SOFT_S_TX 3
+#include "define_sensors.h"
+#include "define_utils.h"
 
 DHT dht(DHTPIN, DHTTYPE); // Initialize DHT sensor for normal 16mhz Arduino
 
@@ -33,8 +25,16 @@ Display displayUtils(&adisplay);
 SoftwareSerial aserial(SOFT_S_RX, SOFT_S_TX); // RX | TX
 Wifi wifi(&aserial);
 
-#define AIR_VALUE 615   //you need to replace this value with Value_1
-#define WATER_VALUE 249 //you need to replace this value with Value_2
+int screenStatus = 0;
+
+unsigned long startTime = millis();
+bool pkgStatus = false;
+int soilmoisturepercent = 0;
+float hum; // use int ?
+float temp;
+bool connected;
+bool resetBtnPrestate;
+bool screenBtnPrestate;
 
 void serialPrintln(String text)
 {
@@ -52,20 +52,119 @@ bool connectToWifi()
   return wifi.isConnected();
 }
 
-void displayStationScreens(bool wifiStatus, bool lastPackageStatus, String soilmoisturepercent, String hum, String temp)
+void displaySensorScreen(String soilmoisturepercent, String hum, String temp)
 {
   displayUtils.clear();
-  displayUtils.addText(0, 0, "WiFi: " + wifiStatus ? "connected" : "disconnected");
-  displayUtils.addText(0, 10, "Last pkg.: " + lastPackageStatus ? "OK" : "ERR");
-  displayUtils.print();
-
-  delay(3000);
-  displayUtils.clear();
-
   displayUtils.addText(0, 0, "Soil: " + soilmoisturepercent + " %");
   displayUtils.addText(0, 10, "Hum: " + hum + " %");
   displayUtils.addText(0, 20, "Temp: " + temp + " C");
   displayUtils.print();
+}
+
+void displayNetworScreen(bool wifiStatus, bool lastPackageStatus)
+{
+  String wifiStatusText = wifiStatus ? "ok" : "err";
+  String lastPkgText = lastPackageStatus ? "ok" : "err";
+
+  displayUtils.clear();
+  displayUtils.addText(0, 0, "WiFi: " + wifiStatusText);
+  displayUtils.addText(0, 10, "Last pkg.: " + lastPkgText);
+  displayUtils.print();
+}
+
+void displayInstantScreen()
+{
+  displayUtils.clear();
+  displayUtils.addText(0, 0, "Instant");
+  displayUtils.print();
+  screenStatus = 0;
+  delay(3000);
+}
+
+void displayResetScreen()
+{
+  displayUtils.clear();
+  displayUtils.addText(0, 0, "Reset");
+  displayUtils.print();
+}
+
+void displayStationScreens(bool wifiStatus, bool lastPackageStatus, String soilmoisturepercent, String hum, String temp)
+{
+  switch (screenStatus)
+  {
+  case 0:
+    displaySensorScreen(soilmoisturepercent, hum, temp);
+    delay(3000);
+    displayNetworScreen(wifiStatus, lastPackageStatus);
+    delay(3000);
+    break;
+  case 1:
+    displayNetworScreen(wifiStatus, lastPackageStatus);
+    break;
+  case 2:
+    displaySensorScreen(soilmoisturepercent, hum, temp);
+    break;
+  case 3: // OFF
+    displayUtils.clear();
+    displayUtils.print();
+    break;
+  case 4:
+    displayInstantScreen();
+    break;
+  case 5:
+    displayResetScreen();
+    break;
+  }
+}
+
+bool checkButton(int pinNumber, bool prestate)
+{
+  int btnState = digitalRead(pinNumber);
+
+  if (btnState == HIGH && prestate == false)
+  {
+    return true;
+  }
+  else if (btnState == LOW)
+  {
+    return false;
+  }
+
+  return false;
+}
+
+void handleButtons()
+{
+  if (checkButton(RESET_BUTTON_PIN, resetBtnPrestate))
+  {
+    resetBtnPrestate = true;
+    screenStatus = 4;
+  }
+  else
+  {
+    resetBtnPrestate = false;
+  }
+
+  if (checkButton(SCREEN_BUTTON_PIN, screenBtnPrestate))
+  {
+    screenStatus++;
+
+    if (screenStatus > 3)
+    {
+      screenStatus = 0;
+    }
+
+    if (resetBtnPrestate)
+    {
+      screenStatus = 5;
+    }
+
+    screenBtnPrestate = true;
+  }
+  else
+  {
+    screenBtnPrestate = false;
+  }
 }
 
 void setup()
@@ -80,6 +179,9 @@ void setup()
 
   connectToWifi();
 
+  pinMode(RESET_BUTTON_PIN, INPUT);
+  pinMode(SCREEN_BUTTON_PIN, INPUT);
+
 #ifndef DEBUG
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
@@ -91,16 +193,9 @@ void setup()
 #endif
 }
 
-unsigned long startTime = millis();
-bool pkgStatus = false;
-int soilmoisturepercent = 0;
-float hum;
-float temp;
-bool connected;
-
 void loop()
 {
-  if ((millis() - startTime) > 600000) // 10 minutes sleep
+  if (((millis() - startTime) > 600000) || resetBtnPrestate == true) // 10 minutes
   {
     startTime = millis();
     int soilMoistureValue = analogRead(A0);
@@ -136,11 +231,11 @@ void loop()
     }
   }
 
+  handleButtons();
+
   displayStationScreens(connected,
                         pkgStatus,
                         String(soilmoisturepercent),
                         String(hum),
                         String(temp));
-
-  // delay(600000); // 10 minutes
 }
